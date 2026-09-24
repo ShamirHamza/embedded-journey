da62e8e964be3c3eee7d5cd14efeed711049b98e
ShamirHamza <shamirhamza25@gmail.com>
2026-09-24 18:40:35 +0000

    W01: keep working fib, remove duplicate

--- host-c/w01/fib.c
+++ host-c/w01/fib.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-
-int fibbonacci(int n) {
-    if (n <= 1) {
-        return n;
+int main (){
+    int x;
+    int a=0;
+    int b=1;
+    printf("Enter the number of terms here:- ");
+    scanf("%d",&x);
+    if (x!=0 && x!=1){
+        printf("%d\n%d\n",a,b);
+    } else {
+        printf("%d\n",a);
+        return 0;
     }
-    return fibbonacci(n - 1) + fibbonacci(n - 2);
-}
-
-int main(){
-    int n;
-    printf("Enter the number of terms: ");
-    scanf("%d", &n);
-    printf("Fibonacci Series: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", fibbonacci(i));
+    for(int i=0;i<(x-2);i++){
+        int fib=a+b;
+        printf("%d\n",fib);
+        a=b;
+        b=fib;
     }
     return 0;
 }
