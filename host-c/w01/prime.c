f13375c81c5607177dd42915700428dacd69ed58
ShamirHamza <shamirhamza25@gmail.com>
2026-09-24 18:28:45 +0000

    W01 D4: prime.c working

--- host-c/w01/prime.c
+++ host-c/w01/prime.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-#include <math.h>
-int main (){
-    int a;
-    printf("Enter the number to be checked here:- ");
-    scanf("%d",&a);
-    int m=2;
-    for (int i=2; i<a; i++){
-        if (a%m==0){
-            printf("The number is not prime!!\n");
-            return 1;
+int main(){
+    int n, i, count = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++){
+        if(n % i == 0){
+            count++;
         }
-        else {
-        printf("The number is prime!!\n");
     }
-    m=m+1;
-    } 
+    if(count == 2){
+        printf("Prime number\n");
+    }
+    else{
+        printf("Not a prime number\n");
+    }
     return 0;
 }
